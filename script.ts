4f4b1b5a0eb4b0cb747d05b0868d0a4ed0b4697f
Shinya Fujino <shf0811@gmail.com>
2022-01-29 19:48:35 +0900

    done

--- script.ts
+++ script.ts
@@ -4,7 +4,10 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
